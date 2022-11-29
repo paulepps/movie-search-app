cc4e4cf6ec7fe897bdfb7d8b7d33a2134dfe160b
VitaliiProfatilov <vitaliiproftl@gmail.com>
2022-11-29 12:59:29 +0200

    commit with gh pages

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ export function App() {
       GenreId, ToDefault, FetchSearch
     }}>
       <Routes>
-        <Route path='movie-search-app/' element={<MainPage 
+        <Route path='/' element={<MainPage 
           movies={movies} 
           sortBy={setSortBy} 
           page={setPage}
